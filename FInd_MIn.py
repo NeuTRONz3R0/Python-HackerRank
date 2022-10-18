6f732243ba37a0b57d0cf4186810fe3d7aa87cd3
Ujjwal-Deep <81875502+NeuTRONz3R0@users.noreply.github.com>
2022-10-18 22:00:29 +0530

    Create FInd_MIn.py

---
+++ FInd_MIn.py
@@ -0,0 +1,12 @@
+A sorted array is rotated at some unknown point, find the minimum element in
+it.
+
+Input: {5, 6, 1, 2, 3, 4}
+Output: 1
+__________________________________________________
+
+import random
+x = [int(x) for x in input("Enter List Elements: ").split()] #Using list comprehension for inputing list elements 
+#random.shuffle(x) #shuffeling because user might enter sorted array 
+print("input: ",x)
+print("output: ",min(x))
